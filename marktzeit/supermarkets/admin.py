02c29602a77bb77c911df40ec14bff40dc375d00
Villiers Strauss <2317574+villiers-s@users.noreply.github.com>
2020-03-21 17:44:58 +0100

    Add model for opening hours
    Refs #7

--- marktzeit/supermarkets/admin.py
+++ marktzeit/supermarkets/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Address, models.Supermarket, models.SupermarketChain)
+@admin.register(
+    models.Address,
+    models.OpeningHours,
+    models.Supermarket,
+    models.SupermarketChain,
+)
 class SupermarketAdmin(admin.ModelAdmin):  # TODO
     pass
