c6a2db915b45d6865fc8ba4522cc9e2c22fa9862
shiwang <s.singh@atenekom.eu>
2020-03-21 20:47:49 +0100

    Implmented Market List and changes styling

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,8 +40,8 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES = {"default": "sqlite:///db.sqlite3"}
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
 # ------------------------------------------------------------------------------
